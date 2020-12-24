5a018147cb44ff4f8cc023632186a80d6e471150
Mark McSherry <mark.mcsherry@gmail.com>
2020-12-24 16:07:53 +0000

    Server Startup and exit
    Morgan http logger not working properly yet

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,42 @@
 //const express = require('express');
 //const server = express();
-const core_server = require("./core_routes"); 
-const port = 8080;
+const morgan = require('morgan'); //HTTP Logger
+const config = require('config'); //Configuration helper
+const server = require("./core_routes"); 
 
-core_server.listen(port, () => {
-  console.log(`Core Server listening at http://localhost:${port}`);
-});
+
+//this.startup();
+
+
+
+//function startup() {
+  console.log('--==LATTICE-CORE SERVER STARTING==--');
+
+  //Setup gracefull shutdown
+  process.on('SIGTERM', shutDown);
+  process.on('SIGINT', shutDown); 
+  console.log('SIGTERM & SIGINT registered to shutdown gracefully...');  
+
+  const morganFormat = config.get('expressSettings.morganFormat');
+  server.use(morgan(morganFormat));
+  console.log('Express HTTP request logger started with mode('+morganFormat+')!');  
+
+  const port = config.get('expressSettings.serverPort');  
+
+  console.log('Express Server starting...');
+  server.listen(port, () => {
+    console.log(`Core Server listening at http://localhost:${port}`);
+  });
+  
+//}
+
+
+console.log('SIGTERM & SIGINT registered to shutdown gracefully...');
+function shutDown() {
+  console.log('Received kill signal, shutting down gracefully');
+  //TODO:Cleanup here if needed
+  //Gracefull shutdown of express not required currently 
+  
+  console.log('Bye Bye...');
+  process.exit(0);
+}
